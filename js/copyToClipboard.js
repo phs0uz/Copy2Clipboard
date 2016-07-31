d3ed6f747f26878e04d0cc812795fc4b5584054e
Margatto <daark.contact@gmail.com>
2016-07-31 14:16:15 -0300

    Update copyToClipboard.js

--- js/copyToClipboard.js
+++ js/copyToClipboard.js
@@ -1,8 +1,38 @@
-function copyToClipboard(txt){	
-        var element = $('<textarea id="' + txt + '">');
-	$('body').append(element);
-	element.val(txt).select();
-	document.execCommand('copy');
-	element.remove();   
+function copyToClipboard(node, txt, textCopied) {
+    var element = document.createElement("TEXTAREA");
+    var span = document.createElement("SPAN");
+
+    var text = document.createTextNode(txt);
+    var copied = document.createTextNode(textCopied);
+
+    var id = 'COPYTOCLIPBOARD_SPAN';
+
+    element.appendChild(text);
+    span.appendChild(copied);
+
+    span.id = id;
+
+    document.body.appendChild(element);
+
+    if (!document.getElementById(id)){
+        document.body.appendChild(span);
+    }
+
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element);
+    span.style.position = 'absolute';
+    span.style.top = node.offsetTop - 30 + 'px';
+    span.style.left = node.offsetLeft + 'px';
+    span.style.opacity = 0;
+    span.style.transition = 'opacity 0.5s linear';
+
+    setTimeout(function () {
+        try {
+            document.body.removeChild(span);
+        }catch(err) {
+            console.log("More than one span permited");
+        }
+    }, 1000);
 }
 
